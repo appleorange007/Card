c9f5203609f5c2c0bbfd4b5ee416ef835999d6cf
George Yao <124420294@qq.com>
2015-06-03 15:46:25 +0800

    Add code.

---
+++ src/main/java/myCard/EmitLog.java
@@ -0,0 +1,40 @@
+package myCard;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] argv)
+            throws java.io.IOException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+        String message = getMessage(argv);
+
+        channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+
+    private static String getMessage(String[] argv) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < argv.length; i++) {
+            sb.append(argv[i]);
+            if (i != argv.length) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
